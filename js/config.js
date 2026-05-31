fa7b4fb5e31846e0140c826726cedfd105c8bc83
nzfrmsnry <37901782+nzfrmsnry@users.noreply.github.com>
2026-06-01 02:18:54 +0800

    Update config.js
    csv links updated

--- js/config.js
+++ js/config.js
@@ -13,10 +13,10 @@ const APP_CONFIG = {
   siteTitle: "New Zealand Masonic Lodge Family Tree",
 
   dataSources: {
-    lodges: "data/lodges.csv",
-    relationships: "data/relationships.csv",
-    amalgamations: "data/amalgamations.csv",
-    timelineEvents: "data/timeline-events.csv"
+    lodges: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3jszO3kCu-oktv5lmJ74qyRWS-F-DJPlfmwEgCzTZ6qtHYiu0R_apMvh3_CZKow/pub?gid=126382585&single=true&output=csv",
+    relationships: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3jszO3kCu-oktv5lmJ74qyRWS-F-DJPlfmwEgCzTZ6qtHYiu0R_apMvh3_CZKow/pub?gid=1469414122&single=true&output=csv",
+    amalgamations: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3jszO3kCu-oktv5lmJ74qyRWS-F-DJPlfmwEgCzTZ6qtHYiu0R_apMvh3_CZKow/pub?gid=708531209&single=true&output=csv",
+    timelineEvents: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR3jszO3kCu-oktv5lmJ74qyRWS-F-DJPlfmwEgCzTZ6qtHYiu0R_apMvh3_CZKow/pub?gid=1344416449&single=true&output=csv"
   },
 
   defaultCrestImage: "images/crests/generic-square-compass.svg",
